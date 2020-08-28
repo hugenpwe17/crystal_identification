5c44e4aeb2555a5bf91eab8e1d34f4f2031164c3
hugenpwe17 <820029885@qq.com>
2020-08-28 23:27:45 +0800

    master

--- tracesource.m
+++ tracesource.m
@@ -1,12 +1,12 @@
-function output = tracesource(input)
+function origin_index = tracesource(Atm_All,Atm_Dat_grain_cell,k)
 %myFun - Description
 %   输入:形变坐标,输出:初始坐标的索引
 % Sycontection output = myFun(input)
 %
 % Long description
-    
-[M,N]=find(Atm_All(:,3:5)==Atm_Dat_grain_cell(8,:,1));
-
-
-
+origin_index=zeros(11,1);    
+for i=1:13
+[M,~]=find(Atm_All(:,3:5)==Atm_Dat_grain_cell(i,:,k));
+temp=max(tabulate(M(:)));
+origin_index(i,1)=temp(1);
 end
